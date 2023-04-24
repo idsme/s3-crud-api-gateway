5deeeeb7ddd755e0e989ce3c3b31fbdd80daa69e
Ids <idsmesales@gmail.com>
2023-04-24 13:57:21 +0200

    Update get.js

--- src/get.js
+++ src/get.js
@@ -4,6 +4,7 @@ const s3 = new AWS.S3();
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 
 module.exports.handler = async (event) => {
+	console.log('received get call');
 	console.log(event);
 
 	const response = {
